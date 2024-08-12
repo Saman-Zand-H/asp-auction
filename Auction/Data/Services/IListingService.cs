61a58da7235f068289cc53bb5118152d0e94fd94
Saman Zand Haghighi <samanzandh@gmail.com>
2024-08-12 18:24:50 +0330

    Add closing bid mechanism

---
+++ Auction/Data/Services/IListingService.cs
@@ -0,0 +1,13 @@
+using Auction.Models;
+
+namespace Auction.Data.Services
+{
+    public interface IListingService
+    {
+        IQueryable<Listing> GetAll();
+        Task Add(Listing listing);
+        Task<Listing?> GetById(int id);
+        Task Delete(int id);
+        Task Update(Listing listing);
+    }
+}
---
+++ Auction/Models/Listing.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace Auction.Models
+{
+    public class Listing : IHasAuthor
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string ImagePath { get; set; } = null!;
+        public bool IsSold { get; set; } = false;
+        public string? IdentityUserId { get; set; }
+        public virtual IdentityUser? IdentityUser { get; set; }
+        public virtual ICollection<Bid>? Bids { get; set; }
+        public virtual ICollection<Comment>? Comments { get; set; }
+
+        public IdentityUser? GetWinner()
+        {
+            if (!IsSold) return null;
+            if (Bids == null || Bids.Count <= 0) return null;
+            return Bids!.OrderByDescending(b => b.Price).First().User;
+        }
+
+        public bool IsUserAuthor(string userId)
+        {
+            return IdentityUserId == userId || IdentityUserId == null;
+        }
+    }
+}
